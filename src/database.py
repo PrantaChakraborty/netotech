69bce6fb64c0cb0fbcd1169b40e18d9e601e8d35
PrantaChakraborty <cbpranta@gmail.com>
2025-09-04 13:52:01 +0600

    Add PostgreSQL connection pooling with AsyncPG

--- src/database.py
+++ src/database.py
@@ -1,65 +1,84 @@
 import contextlib
 from typing import Any, AsyncIterator
 
-from src.config import settings
+import asyncpg
 from sqlalchemy.ext.asyncio import (
-    AsyncConnection,
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
+  AsyncConnection,
+  AsyncSession,
+  async_sessionmaker,
+  create_async_engine,
 )
 from sqlalchemy.orm import declarative_base
 
+from src.config import settings
 
 
 class DatabaseSessionManager:
-    def __init__(self, host: str, engine_kwargs: dict[str, Any] = {}):
-        self._engine = create_async_engine(host, **engine_kwargs)
-        self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine)
+  def __init__(self, host: str, engine_kwargs: dict[str, Any] = {}):
+    self._engine = create_async_engine(host, **engine_kwargs)
+    self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine)
 
-    async def close(self):
-        if self._engine is None:
-            raise Exception("DatabaseSessionManager is not initialized")
-        await self._engine.dispose()
+  async def close(self):
+    if self._engine is None:
+      raise Exception("DatabaseSessionManager is not initialized")
+    await self._engine.dispose()
 
-        self._engine = None
-        self._sessionmaker = None
+    self._engine = None
+    self._sessionmaker = None
 
-    @contextlib.asynccontextmanager
-    async def connect(self) -> AsyncIterator[AsyncConnection]:
-        if self._engine is None:
-            raise Exception("DatabaseSessionManager is not initialized")
+  @contextlib.asynccontextmanager
+  async def connect(self) -> AsyncIterator[AsyncConnection]:
+    if self._engine is None:
+      raise Exception("DatabaseSessionManager is not initialized")
 
-        async with self._engine.begin() as connection:
-            try:
-                yield connection
-            except Exception:
-                await connection.rollback()
-                raise
+    async with self._engine.begin() as connection:
+      try:
+        yield connection
+      except Exception:
+        await connection.rollback()
+        raise
 
-    @contextlib.asynccontextmanager
-    async def session(self) -> AsyncIterator[AsyncSession]:
-        if self._sessionmaker is None:
-            raise Exception("DatabaseSessionManager is not initialized")
+  @contextlib.asynccontextmanager
+  async def session(self) -> AsyncIterator[AsyncSession]:
+    if self._sessionmaker is None:
+      raise Exception("DatabaseSessionManager is not initialized")
 
-        session = self._sessionmaker()
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+    session = self._sessionmaker()
+    try:
+      yield session
+    except Exception:
+      await session.rollback()
+      raise
+    finally:
+      await session.close()
 
-SQL_ALCHEMY_DB_URL = (f"postgresql+asyncpg://{settings.DB_USER}:"
-                      f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
-                      f"{settings.DB_PORT}/{settings.DB_NAME}")
 
+SQL_ALCHEMY_DB_URL = (
+  f"postgresql+asyncpg://{settings.DB_USER}:"
+  f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
+  f"{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+ASYNC_DB_URL = (
+  f"postgresql://{settings.DB_USER}:"
+  f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
+  f"{settings.DB_PORT}/{settings.DB_NAME}"
+)
 sessionmanager = DatabaseSessionManager(SQL_ALCHEMY_DB_URL)
 
 Base = declarative_base()
 
 
 async def get_db():
-    async with sessionmanager.session() as session:
-        yield session
+  async with sessionmanager.session() as session:
+    yield session
+
+
+pool: asyncpg.Pool | None = None
+
+
+async def get_pg_pool() -> asyncpg.Pool:
+  global pool
+  if pool is None:
+    pool = await asyncpg.create_pool(ASYNC_DB_URL)
+  return pool
